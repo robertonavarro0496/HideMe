b6a3056293fbc773fbfdfb2dce27f80204211ea3
BatDr0id <dr1197dr@gmail.com>
2018-04-15 20:51:16 -0400

    Key entered now works

--- HideMe/app/src/main/java/com/example/sempiternalsearch/hideme2/Login.java
+++ HideMe/app/src/main/java/com/example/sempiternalsearch/hideme2/Login.java
@@ -2,6 +2,7 @@ package com.example.sempiternalsearch.hideme2;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -13,10 +14,14 @@ public class Login extends AppCompatActivity {
     Context context;
     Button button1, button2, button3, button4, button5, button6, button7, button8, button9, button0, login;
     EditText editText;
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
     private int[] key;
     protected void onCreate(Bundle saved){
         super.onCreate(saved);
         setContentView(R.layout.login);
+        pref = getApplicationContext().getSharedPreferences("MyPref", 0);
+        editor = pref.edit();
         context = this;
         editText = (EditText) findViewById(R.id.editText2);
         button1 = (Button) findViewById(R.id.button1);
@@ -41,9 +46,13 @@ public class Login extends AppCompatActivity {
     }
     public void buttonLogin(View v){
         if (editText.length() >= 4){
-            Intent intent = new Intent(context, Gallery.class);
-            startActivity(intent);
+            if(Integer.parseInt(editText.getText().toString()) == pref.getInt("key", -1)){
+                Intent intent = new Intent(context, Gallery.class);
+                startActivity(intent);
+            }
+
         }
+        else {}
     }
     private void buttonAppend(String buttonString){
         String editString = editText.getText().toString();
